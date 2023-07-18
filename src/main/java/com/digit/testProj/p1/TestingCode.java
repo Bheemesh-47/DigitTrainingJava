04ebc9bd1842ce88127891b5bd6a1ef74b8ff9bc
Bheemesh-47 <71122446+Bheemesh-47@users.noreply.github.com>
2023-07-18 12:40:53 +0530

    Update TestingCode.java

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -5,9 +5,8 @@ public class TestingCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi From Sachin");
-		System.out.println("Hi From Sachin");
-		
-		System.out.println("Hi From Dhoni");
+
+
 		System.out.println("Hi From Dhoni");
 
 	}
