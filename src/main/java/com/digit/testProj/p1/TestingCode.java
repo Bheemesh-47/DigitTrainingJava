0b291079a61930367d77e66ce08de80ca8bba111
Employee <Employee@SL-LAP-1916>
2023-07-18 12:23:32 +0530

    This is my first commit

---
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -0,0 +1,13 @@
+package com.digit.testProj.p1;
+
+public class TestingCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi From Sachin");
+		System.out.println("Hi From Sachin");
+		System.out.println("Hi From Sachin");
+		System.out.println("Hi From Sachin");
+	}
+
+}
